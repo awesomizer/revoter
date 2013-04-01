ee992722131700f576a7fef6bf9958c051bac902
Christian Smith-Socaris <christian.working@gmail.com>
2013-03-31 22:55:10 -0400

    pushing unauthenticated users to sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Votevote::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  unauthenticated do
+    devise_scope :user do
+     root :to => "devise/registrations#new"
+    end
+  end
   devise_for :users
   resources :users
 end
