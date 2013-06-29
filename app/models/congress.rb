3a3e66793a53be8be27bfec7305b5254ac3a7ab9
Christian Smith-Socaris <christian.working@gmail.com>
2013-06-29 18:48:27 -0400

    new model name

---
+++ app/models/congress.rb
@@ -0,0 +1,25 @@
+class CongressApi
+  include HTTParty
+  debug_output $stdout
+  base_uri 'http://congress.api.sunlightfoundation.com'
+
+  def initialize
+  end
+
+  def bill type, number, congress
+    bill_id = type + number + '-' + congress
+    options = {query: {apikey: ENV["SUNLIGHT_KEY"], bill_id: bill_id, fields: 'votes,history'}}
+      self.class.get('/bills', options)
+  end
+
+  def votes roll_id
+    options = {query: {apikey: ENV["SUNLIGHT_KEY"], roll_id: roll_id}}
+      self.class.get('/votes', options)
+  end
+
+  def legislators
+  end
+
+end
+
+
