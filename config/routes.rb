86885a8e17530e4cdefe009eff1b0503820d6316
Christian Smith-Socaris <christian.working@gmail.com>
2013-06-30 15:39:45 -0400

    remove auth routes, add bill find route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Votevote::Application.routes.draw do
-  authenticated :user do
+  unauthenticated :user do
     root :to => 'home#index'
   end
-  unauthenticated do
-    devise_scope :user do
-     root :to => "devise/registrations#new"
-    end
-  end
+#  unauthenticated do
+    #devise_scope :user do
+     #root :to => "devise/registrations#new"
+    #end
+  #end
   devise_for :users
   resources :users
+  
+  # congress_api matchers
+  get '/congress_api/bill', to: 'congress_api#bill', as: :bill
 end
