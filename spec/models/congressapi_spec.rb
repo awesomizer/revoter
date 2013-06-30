65f831b105421b5974051f7325657194bd76da68
Christian Smith-Socaris <christian.working@gmail.com>
2013-06-30 00:21:48 -0400

    fix response expectation for vote method

--- spec/models/congressapi_spec.rb
+++ spec/models/congressapi_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 
 describe CongressApi do
 
+  # Mock the api in these tests?
+
   it 'should retrieve bill information with valid bill info' do
       api = CongressApi.new 
       response = api.bill('hr', '3590', '111')
@@ -18,7 +20,14 @@ describe CongressApi do
   it 'should retrieve vote information with valid roll_id' do
       api = CongressApi.new
       response = api.votes('s396-2009')
-      expect(response.parsed_response["results"][0]["bill_id"]).not_to eq(nil)
+      expect(response.parsed_response["results"][0]["question"]).not_to eq(nil)
   end
+
+  it 'should retrieve legislator information with a valid bioguide_id' do
+      api = CongressApi.new
+      response = api.legislators('B000944')
+      expect(response.parsed_response["results"][0]["state"]).not_to eq(nil)
+  end
+
 
 end
