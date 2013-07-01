e4554e205ea9250a1c5ecfd968421f039deeeb50
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-01 03:28:33 -0400

    cosmetic

--- app/models/congress_api.rb
+++ app/models/congress_api.rb
@@ -8,18 +8,27 @@ class CongressApi
 
   def find_bill type, number, congress
     bill_id = type + number + '-' + congress
-    options = {query: {apikey: ENV["SUNLIGHT_KEY"], bill_id: bill_id, fields: 'last_vote_at,titles,nicknames,summary_short,urls,votes,history,last_action'}}
-      self.class.get('/bills', options)
+    options = {query: 
+               {apikey: ENV["SUNLIGHT_KEY"], 
+                bill_id: bill_id, 
+                fields: 'last_vote_at,official_title,titles,nicknames,summary_short,urls,votes,history,last_action'}}
+      self.class.get('/bills', options).parsed_response
   end
 
   def votes roll_id
-    options = {query: {apikey: ENV["SUNLIGHT_KEY"], roll_id: roll_id, fields: 'vote_type,question,required,voter_ids,voters,result'}}
-      self.class.get('/votes', options)
+    options = {query: 
+               {apikey: ENV["SUNLIGHT_KEY"], 
+                roll_id: roll_id, 
+                fields: 'vote_type,question,required,voter_ids,voters,result'}}
+      self.class.get('/votes', options).parsed_response
   end
 
   def legislators bioguide_id
-    options = {query: {apikey: ENV["SUNLIGHT_KEY"], bioguide_id: bioguide_id, fields: 'state,title,chamber,state_rank,term_start,term_end,first_name,nickname,last_name'}}
-      self.class.get('/legislators', options)
+    options = {query: 
+               {apikey: ENV["SUNLIGHT_KEY"], 
+                bioguide_id: bioguide_id, 
+                fields: 'state,title,chamber,state_rank,term_start,term_end,first_name,nickname,last_name'}}
+      self.class.get('/legislators', options).parsed_response
   end
 
 end
