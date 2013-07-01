05c29e1bacf397e4b66439605d7d12cab3aa7987
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-01 14:50:56 -0400

    add bill structure

--- app/controllers/congress_api_controller.rb
+++ app/controllers/congress_api_controller.rb
@@ -1,9 +1,21 @@
 class CongressApiController < ApplicationController
 
+    Bill = Struct.new( 
+      :titles, 
+      :official_title, 
+      :nickname, 
+      :summary_short, 
+      :last_vote_at, 
+      :last_action, 
+      :history, 
+      :votes, 
+      :urls)
+
   def bill
     api = CongressApi.new
-    @bill = api.find_bill(params[:bill_type], params[:bill_number], params[:congress])
-    @bill = @bill["results"][0]
+    results = api.find_bill(params[:bill_type], params[:bill_number], params[:congress])
+    results = results["results"][0]
+    @bill = Bill.new(results["titles"], results["official_title"], results["nickname"], results["summary_short"], results["last_vote_at"], results["last_action"], results["history"], results["votes"], results["urls"])
   end
   
   def votes
