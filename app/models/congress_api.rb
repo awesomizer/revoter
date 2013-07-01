e83a798badb869baf18ec3457cf7cee23a7e14c7
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-01 19:39:51 -0400

    change fields in api requests

--- app/models/congress_api.rb
+++ app/models/congress_api.rb
@@ -11,15 +11,15 @@ class CongressApi
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
                 bill_id: bill_id, 
-                fields: 'last_vote_at,official_title,titles,nicknames,summary_short,urls,votes,history,last_action'}}
-      self.class.get('/bills', options).parsed_response
+                fields: 'last_vote_at,official_title,nicknames,summary_short,votes,urls,last_action'}}
+      self.class.get('/bills', options) #.parsed_response
   end
 
   def votes roll_id
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
                 roll_id: roll_id, 
-                fields: 'vote_type,question,required,voter_ids,voters,result'}}
+                fields: 'roll_id,vote_type,question,required,voter_ids,voters,result'}}
       self.class.get('/votes', options).parsed_response
   end
 
