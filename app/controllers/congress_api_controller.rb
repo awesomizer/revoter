4efb8f352b50e5ee9c177f9501a1bad87759f8bf
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-02 01:25:07 -0400

    create votes instance variable in bill controller action

--- app/controllers/congress_api_controller.rb
+++ app/controllers/congress_api_controller.rb
@@ -3,15 +3,28 @@ class CongressApiController < ApplicationController
 
   def bill
     api = CongressApi.new
-    results = api.find_bill(params[:bill_type], params[:bill_number], params[:congress])
-    results = results["results"][0]
-    @bill = Bill.new(official_title: results["official_title"], 
-                     nicknames: results["nickname"], 
-                     summary_short: results["summary_short"], 
-                     last_vote_at: results["last_vote_at"], 
-                     last_action: results["last_action"], 
-                     vote_list: results["votes"],
-                     urls: results["urls"])
+    bill_results = api.get_bill(params[:bill_type], params[:bill_number], params[:congress])
+    bill_results = bill_results["results"][0]
+    @bill = Bill.new(official_title: bill_results["official_title"], 
+                     nicknames: bill_results["nickname"], 
+                     summary_short: bill_results["summary_short"], 
+                     last_vote_at: bill_results["last_vote_at"], 
+                     last_action: bill_results["last_action"], 
+                     vote_list: bill_results["votes"],
+                     urls: bill_results["urls"])
+    @bill.vote_list.each do |v|
+      @votes = []
+      vote_results = api.get_vote(v["roll_id"])
+      vote_results = vote_results["results"][0]
+      @votes << Vote.new(question: vote_results["question"], 
+                           required: vote_results["required"], 
+                           result: vote_results["result"], 
+                           roll_id: vote_results["roll_id"], 
+                           vote_type: vote_results["vote_type"],
+                           breakdown: vote_results["breakdown"],
+                           voter_ids: vote_results["voter_ids"], 
+                           bill_id: @bill.id)
+    end
   end
   
   def votes
