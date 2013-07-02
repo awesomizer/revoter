321e371cec54c6805be1a665a0431da88cc8976b
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-02 01:25:57 -0400

    rename congress api methods for clarity

--- app/models/congress_api.rb
+++ app/models/congress_api.rb
@@ -6,24 +6,24 @@ class CongressApi
   def initialize
   end
 
-  def find_bill type, number, congress
+  def get_bill type, number, congress
     bill_id = type + number + '-' + congress
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
                 bill_id: bill_id, 
                 fields: 'last_vote_at,official_title,nicknames,summary_short,votes,urls,last_action'}}
-      self.class.get('/bills', options) #.parsed_response
+      self.class.get('/bills', options).parsed_response
   end
 
-  def votes roll_id
+  def get_vote roll_id
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
                 roll_id: roll_id, 
-                fields: 'roll_id,vote_type,question,required,voter_ids,voters,result'}}
+                fields: 'roll_id,vote_type,question,required,breakdown,voter_ids,voters,result'}}
       self.class.get('/votes', options).parsed_response
   end
 
-  def legislators bioguide_id
+  def get_legislator bioguide_id
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
                 bioguide_id: bioguide_id, 
