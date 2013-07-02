8bf4e9001d8386dcc06c558e0e754b8cc19fd402
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-02 01:26:21 -0400

    add breakdown to attr_accessible for vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :question, :required, :result, :roll_id, :vote_type, :voter_ids, :bill_id
+  attr_accessible :question, :required, :result, :roll_id, :vote_type, :breakdown, :voter_ids, :bill_id
 
   belongs_to :bill
 end
