7f0d2eed427f32404ab04bd042c66c0844b1d79d
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-03 19:59:29 -0400

    add fields to bill api query, change new to create for instance gen

--- app/controllers/congress_api_controller.rb
+++ app/controllers/congress_api_controller.rb
@@ -5,22 +5,26 @@ class CongressApiController < ApplicationController
     api = CongressApi.new
     bill_results = api.get_bill(params[:bill_type], params[:bill_number], params[:congress])
     bill_results = bill_results["results"][0]
-    @bill = Bill.new(official_title: bill_results["official_title"], 
-                     nicknames: bill_results["nickname"], 
-                     summary_short: bill_results["summary_short"], 
-                     last_vote_at: bill_results["last_vote_at"], 
-                     last_action: bill_results["last_action"], 
-                     vote_list: bill_results["votes"],
-                     urls: bill_results["urls"])
-      @votes = votes(@bill.vote_list)
+    @bill = Bill.create(sun_bill_id: bill_results["bill_id"],
+                        official_title: bill_results["official_title"], 
+                        nicknames: bill_results["nickname"], 
+                        summary_short: bill_results["summary_short"], 
+                        last_vote_at: bill_results["last_vote_at"], 
+                        last_action: bill_results["last_action"], 
+                        vote_list: bill_results["votes"],
+                        urls: bill_results["urls"])
+      @votes = votes(api, @bill.vote_list)
   end
   
-  def votes vote_list
+
+  private
+
+  def votes api, vote_list
     vote_list.each do |v|
       @votes = []
       vote_results = api.get_vote(v["roll_id"])
       vote_results = vote_results["results"][0]
-      @votes << Vote.new(question: vote_results["question"], 
+      @votes << Vote.create(question: vote_results["question"], 
                            required: vote_results["required"], 
                            result: vote_results["result"], 
                            roll_id: vote_results["roll_id"], 
@@ -28,6 +32,15 @@ class CongressApiController < ApplicationController
                            breakdown: vote_results["breakdown"],
                            voter_ids: vote_results["voter_ids"], 
                            bill_id: @bill.id)
+    end
+  end
+
+  def roll_call api, voter_ids
+      @roll_call = []
+      legislator_results = api.roll_call(voter_ids)
+      legislator_results = legislator_results["results"][0]
+      @roll_call << Legislator.create(bioguide_id: legislator_results["bioguide_id"],
+                                      
     end
   end
 
