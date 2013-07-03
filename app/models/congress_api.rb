e5741b7bccf5afbafb4fa68760ebff29c35ac505
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-03 19:59:52 -0400

    add fields to api query

--- app/models/congress_api.rb
+++ app/models/congress_api.rb
@@ -7,11 +7,11 @@ class CongressApi
   end
 
   def get_bill type, number, congress
-    bill_id = type + number + '-' + congress
+    sun_bill_id = type + number + '-' + congress
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
-                bill_id: bill_id, 
-                fields: 'last_vote_at,official_title,nicknames,summary_short,votes,urls,last_action'}}
+                bill_id: sun_bill_id, 
+                fields: 'bill_id,last_vote_at,official_title,nicknames,summary_short,votes,urls,last_action'}}
       self.class.get('/bills', options).parsed_response
   end
 
@@ -27,11 +27,11 @@ class CongressApi
     options = {query: 
                {apikey: ENV["SUNLIGHT_KEY"], 
                 bioguide_id: bioguide_id, 
-                fields: 'state,title,chamber,state_rank,term_start,term_end,first_name,nickname,last_name'}}
+                fields: 'bioguide_id,state,title,chamber,state_rank,term_start,term_end,first_name,nickname,last_name'}}
       self.class.get('/legislators', options).parsed_response
   end
 
-  def roll_call
+  def roll_call voter_ids
     # list of voters with vote and individual info
   end
 end
