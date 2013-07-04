4069dc22ab851cc9ca24125f08a00d506f2a8a74
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-04 17:14:11 -0400

    cosmetic

--- app/controllers/congress_api_controller.rb
+++ app/controllers/congress_api_controller.rb
@@ -3,16 +3,16 @@ class CongressApiController < ApplicationController
 
   def bill
     api = CongressApi.new
-    bill_results = api.get_bill(params[:bill_type], params[:bill_number], params[:congress])
-    bill_results = bill_results["results"][0]
-    @bill = Bill.find_or_create_by(sun_bill_id: bill_results["bill_id"]) do |b|
-                        b.official_title: bill_results["official_title"], 
-                        b.nicknames: bill_results["nickname"], 
-                        b.summary_short: bill_results["summary_short"], 
-                        b.last_vote_at: bill_results["last_vote_at"], 
-                        b.last_action: bill_results["last_action"], 
-                        b.vote_list: bill_results["votes"],
-                        b.urls: bill_results["urls"])
+    results = api.get_bill(params[:bill_type], params[:bill_number], params[:congress])
+    results = bill_results["results"][0]
+    @bill = Bill.find_or_create_by(sun_bill_id: results["bill_id"]) do |b|
+                        b.official_title: results["official_title"], 
+                        b.nicknames: results["nickname"], 
+                        b.summary_short: results["summary_short"], 
+                        b.last_vote_at: results["last_vote_at"], 
+                        b.last_action: results["last_action"], 
+                        b.vote_list: results["votes"],
+                        b.urls: results["urls"])
     end
       @votes = votes(@bill.vote_list)
   end
@@ -24,14 +24,14 @@ class CongressApiController < ApplicationController
     api = CongressApi.new
     vote_list.each do |v|
       @votes = []
-      vote_results = api.get_vote(v["roll_id"])
-      vote_results = vote_results["results"][0]
-      @votes << Vote.find_or_create_by(roll_id: vote_results["roll_id"]) do |v|
-                          v.question: vote_results["question"], 
-                          v.required: vote_results["required"], 
-                          v.result: vote_results["result"], 
-                          v.vote_type: vote_results["vote_type"],
-                          v.breakdown: vote_results["breakdown"],
+      results = api.get_vote(v["roll_id"])
+      results = vote_results["results"][0]
+      @votes << Vote.find_or_create_by(roll_id: results["roll_id"]) do |v|
+                          v.question: results["question"], 
+                          v.required: results["required"], 
+                          v.result: results["result"], 
+                          v.vote_type: results["vote_type"],
+                          v.breakdown: results["breakdown"],
                           v.roll_call: roll_call(vote_results["voter_ids"]), 
                           v.bill_id: @bill.id)
       end
@@ -41,11 +41,17 @@ class CongressApiController < ApplicationController
   def roll_call voter_ids
     api = CongressApi.new
     roll_call = []
-    legislator_results = api.roll_call(voter_ids)
-    legislator_results = legislator_results["results"][0]
-    roll_call << Legislator.create(bioguide_id: legislator_results["bioguide_id"],
-                                      
+    voter_ids.each do |id|
+      results = api.get_legislator(id.key)
+      results = results["results"][0]
+      roll_call << {vote_record: {bioguide_id: results[id.key],
+                    state: results["state"],
+                    state_rank: results["state_rank"],
+                    first_name: results["first_name"],
+                    nickname: results["nickname"],
+                    last_name: results["last_name"]}}
     end
+    roll_call
   end
 
 end
