f3f85364a281f623e0c7495f937dff52cf3beb1e
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-04 17:13:08 -0400

    add roll_call to accessible attributes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,6 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :question, :required, :result, :roll_id, :vote_type, :breakdown, :voter_ids, :bill_id
+  attr_accessible :question, :required, :result, :roll_id, :vote_type, :breakdown, :voter_ids, :bill_id, :roll_call
 
   belongs_to :bill
+  has_and_belongs_to_many :legislators
 end
