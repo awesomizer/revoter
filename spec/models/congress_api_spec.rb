25be35349a3d04972c7e1d0ce0eeca0644f75828
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-04 17:49:25 -0400

    change congress_api model test to reflect method changes

--- spec/models/congress_api_spec.rb
+++ spec/models/congress_api_spec.rb
@@ -7,26 +7,26 @@ describe CongressApi do
 
   it 'should retrieve bill information with valid bill info' do
       api = CongressApi.new 
-      response = api.find_bill('hr', '3590', '111')
-      expect(response.parsed_response["results"][0]["bill_id"]).to eq('hr3590-111')
+      response = api.get_bill('hr', '3590', '111')
+      expect(response["results"][0]["bill_id"]).to eq('hr3590-111')
   end
 
   it 'should display an error message with invalid bill info' do
       api = CongressApi.new 
-      response = api.find_bill('hr', '3590', 'xxx')
+      response = api.get_bill('hr', '3590', 'xxx')
       expect(response).to raise_error
   end
   
   it 'should retrieve vote information with valid roll_id' do
       api = CongressApi.new
-      response = api.votes('s396-2009')
-      expect(response.parsed_response["results"][0]["question"]).not_to eq(nil)
+      response = api.get_vote('s396-2009')
+      expect(response["results"][0]["question"]).not_to eq(nil)
   end
 
   it 'should retrieve legislator information with a valid bioguide_id' do
       api = CongressApi.new
-      response = api.legislators('B000944')
-      expect(response.parsed_response["results"][0]["state"]).not_to eq(nil)
+      response = api.get_legislator('B000944')
+      expect(response["results"][0]["state"]).not_to eq(nil)
   end
 
 
