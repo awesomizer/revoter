5bf1902632d522e724b9f543ccb0fb25caa17573
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-08 14:33:03 -0400

    Bill factory

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -1,12 +1,30 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+vote_list_array = [
+  {
+    "acted_at"=>"2013-06-27", 
+    "how"=>"roll", 
+    "references"=>[
+      {"reference"=>"CR S5357-5475", 
+       "type"=>"text"}], 
+    "result"=>"pass", 
+    "text"=>"Passed Senate with an amendment by Yea-Nay Vote. 68 - 32. Record Vote Number: 168.", 
+    "type"=>"vote", 
+    "vote_type"=>"vote", 
+    "chamber"=>"senate", 
+    "roll_id"=>"s168-2013"
+  }
+]
+
 FactoryGirl.define do
   factory :bill do
-    official_title "MyString"
-    summary_short "MyText"
+    official_title "Best Bill"
+    summary_short "Awesomeness, thanks."
     last_vote_at "2013-07-01 16:35:11"
-    last_action "MyString"
-    nicknames "MyText"
-    urls "MyText"
+    last_action "Winning"
+    nicknames "#1 Bill"
+    urls "http://www.bestbill.com"
+    sun_bill_id "s744-113"
+    vote_list vote_list_array
   end
 end
