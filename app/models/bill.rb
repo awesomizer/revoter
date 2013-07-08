e0626213249c5b8c7993f90562fee7c01decf0b5
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-08 14:30:53 -0400

    serialize hash attributes

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,9 @@
 class Bill < ActiveRecord::Base
+
+  serialize :nicknames
+  serialize :vote_list
+  serialize :urls
+
   attr_accessible :sun_bill_id, :last_action, :last_vote_at, :nicknames, :official_title, :summary_short, :vote_list, :urls
 
   has_many :votes
