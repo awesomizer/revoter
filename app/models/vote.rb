ca4668d7d77fe30c757b24b3f339f75dfda0df6b
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-08 23:58:40 -0400

    add voters attribute

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,9 @@ class Vote < ActiveRecord::Base
 
   serialize :breakdown
   serialize :voter_ids
+  serialize :voters
 
-  attr_accessible :question, :required, :result, :roll_id, :vote_type, :breakdown, :voter_ids, :bill_id
+  attr_accessible :question, :required, :result, :roll_id, :vote_type, :breakdown, :voter_ids, :voters, :bill_id
 
   belongs_to :bill
   has_and_belongs_to_many :legislators
