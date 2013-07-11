517e64be36145e37079e574e2d956a6135e9305e
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-11 15:30:03 -0400

    fix HABTM association test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Vote do
   it {should belong_to(:bill)}
-  it {should have_and_belong_to(:legislators)}
+  it {should have_and_belong_to_many(:legislators)}
   it {should validate_presence_of(:roll_id)}
+  it {should validate_uniqueness_of(:roll_id)}
 end
