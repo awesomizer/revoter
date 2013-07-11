08c53a730386a95c3efbb3596865e93fae265507
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-11 14:12:34 -0400

    add validation on sun_bill_id

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -7,4 +7,6 @@ class Bill < ActiveRecord::Base
   attr_accessible :sun_bill_id, :last_action, :last_vote_at, :nicknames, :official_title, :summary_short, :vote_list, :urls
 
   has_many :votes
+
+  validates_presence_of :sun_bill_id, :uniqueness => :true
 end
