f78c06dcdbe499e584194054fa160d58e0521b00
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-11 14:13:04 -0400

    add associations and validations to model tests

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Bill do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many(:votes)}
+  it {should validate_presence_of(:sun_bill_id)}
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Vote do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:bill)}
+  it {should have_and_belong_to(:legislators)}
+  it {should validate_presence_of(:roll_id)}
 end
