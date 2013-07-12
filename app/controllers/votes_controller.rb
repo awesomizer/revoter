50ac126f75a59ed5dd2e0988fd3b8a7d8135eedd
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-12 13:33:44 -0400

    add alternative votes to show method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,9 @@ class VotesController < ApplicationController
 
   def show
     @vote = Vote.find(params[:id])
-    @pop_votes, @pop_tally = Vote.pop_weighted(@vote.voters)
+    @pop_votes, @pop_tally = Vote.fractional_vote(@vote.voters, :percent)
+    @one_per_frac_votes, @one_per_frac_tally = Vote.fractional_vote(@vote.voters, :one_per)
+    @one_per_int_votes, @one_per_int_tally = Vote.integer_vote(@vote.voters, :one_per)
   end
 
 end
