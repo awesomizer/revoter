f5bbe063fc941f5640751b0f8c2deb83e53d22f9
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-12 16:38:45 -0400

    add required percentage to params

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,9 +2,9 @@ class VotesController < ApplicationController
 
   def show
     @vote = Vote.find(params[:id])
-    @pop_votes, @pop_tally = Vote.fractional_vote(@vote.voters, :percent)
-    @one_per_frac_votes, @one_per_frac_tally = Vote.fractional_vote(@vote.voters, :one_per)
-    @one_per_int_votes, @one_per_int_tally = Vote.integer_vote(@vote.voters, :one_per)
+    @pop_votes, @pop_tally = Vote.fractional_vote(@vote.voters, :percent, @vote.required)
+    @one_per_frac_votes, @one_per_frac_tally = Vote.fractional_vote(@vote.voters, :one_per, @vote.required)
+    @one_per_int_votes, @one_per_int_tally = Vote.integer_vote(@vote.voters, :one_per, @vote.required)
   end
 
 end
