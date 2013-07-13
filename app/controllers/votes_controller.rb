fc0199f6164c26e89100a80491bf3d34939e45bf
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-13 18:51:04 -0400

    assign gon variables in show action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,9 @@ class VotesController < ApplicationController
     @pop_votes, @pop_tally = Vote.fractional_vote(@vote.voters, :percent, @vote.required)
     @one_per_frac_votes, @one_per_frac_tally = Vote.fractional_vote(@vote.voters, :one_per, @vote.required)
     @one_per_int_votes, @one_per_int_tally = Vote.integer_vote(@vote.voters, :one_per, @vote.required)
+    gon.pop_votes, gon.pop_tally = @pop_votes, @pop_tally
+    gon.one_per_frac_votes, gon.one_per_frac_tally = @one_per_frac_votes, @one_per_frac_tally
+    gon.one_per_int_votes, gon.one_per_int_tally = @one_per_int_votes, @one_per_int_tally
   end
 
 end
