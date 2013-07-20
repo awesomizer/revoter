cec9ddf1dc593ae203a16708264ae149979a456f
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-19 23:23:07 -0400

    add names to non-resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Votevote::Application.routes.draw do
   
   # controller matchers
   get '/bill', to: 'bills#find', as: :find_bill
-  get '/bills/:id', to: 'bills#show'
-  get '/votes/:id', to: 'votes#show'
+  get '/bills/:id', to: 'bills#show', as: :bill
+  get '/votes/:id', to: 'votes#show', as: :vote
 end
