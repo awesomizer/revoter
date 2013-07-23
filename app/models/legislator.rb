4c20117c408a0229e20aff19d40d6c0e19016c8c
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-22 22:49:34 -0400

    legislator model attribute changes

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,17 +1,20 @@
 class Legislator < ActiveRecord::Base
 
   attr_accessible :bioguide_id,
-    :state,
+    :state_code,
     :title,
     :chamber,
     :state_rank,
     :state_name,
-    :term_start,
+    :term_start, # term_start and term_end are not part of the voter record that is being used to create legislators
     :term_end,
     :first_name,
     :nickname,
-    :last_name
+    :last_name,
+    :party,
+    :state_id
 
+  belongs_to :state
   has_and_belongs_to_many :votes
 
 end
