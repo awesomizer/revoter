edf2c0a19b03bdf9fc1dc805083f0fd4a8914f2b
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-22 22:48:58 -0400

    votes helpers

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -8,4 +8,37 @@ module VotesHelper
     end
   end
     
+  def get_legislator voter
+    @vote.legislators.find_by_bioguide_id(voter["bioguide_id"])
+  end
+
+#  def vote_weight legislator
+    
+  def roll_call_list party, position, *roll
+    list = []
+    @vote.voters.each_value do |v|
+      if v["voter"]["party"] == party && v["vote"] == position
+        legislator = get_legislator(v["voter"])
+        item = roll_call_item(legislator, roll)
+        list << item
+      end
+    end
+    list
+  end
+
+  def roll_call_item legislator, *roll
+    case roll[0][0]
+    when 'traditional'
+      "#{legislator.last_name}, #{legislator.first_name}, #{legislator.state_name} / #{legislator.state_rank} trad"
+    when 'popular'
+      "#{legislator.last_name}, #{legislator.first_name}, #{legislator.state_name} / #{legislator.state_rank} pop"
+    when 'fractional'
+      "#{legislator.last_name}, #{legislator.first_name}, #{legislator.state_name} / #{legislator.state_rank} frac"
+    when 'seniority'
+      "#{legislator.last_name}, #{legislator.first_name}, #{legislator.state_name} / #{legislator.state_rank} sen"
+    else
+      "#{legislator.last_name}, #{legislator.first_name}, #{legislator.state_name} / #{legislator.state_rank} else"
+    end
+  end
+
 end
