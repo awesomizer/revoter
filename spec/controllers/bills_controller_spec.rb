cc785ef48cdb67214e1648c07a8f3731c953ab64
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-26 01:27:47 -0400

    add basic variable assignment test for bill controller

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -10,12 +10,12 @@ describe BillsController do
       assigns(:bill).should eq(bill)
     end
 
-#    it 'retrieves an array of vote information and assign it to an instance variable' do
-      #vote1 = FactoryGirl.create(:vote)
-      #vote2 = FactoryGirl.create(:vote)
-      #get :bill, {bill_type: "hr", bill_number: "3590", congress: "111"}
-      #assigns(:votes).size.should > 1
-    #end
+    it 'retrieves an array of vote information and assign it to an instance variable' do
+      vote1 = FactoryGirl.create(:vote)
+      vote2 = FactoryGirl.create(:vote)
+      get :bill, {bill_type: "hr", bill_number: "3590", congress: "111"}
+      assigns(:votes).size.should > 1
+    end
 
   end
 end
