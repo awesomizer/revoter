b167ec914a386e87b6c288aaa7e1d6075173709d
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-26 03:22:02 -0400

    legislator factory

---
+++ spec/factories/legislators.rb
@@ -0,0 +1,15 @@
+FactoryGirl.describe do
+  factory :legislator do
+    id 1
+    bioguide_id 'A000'
+    state_code 'AK'
+    title 'Senator'
+    chamber 'senate'
+    state_rank 'senior'
+    state_name 'Alaska'
+    first_name 'Sen'
+    last_name 'Ator'
+    party 'D'
+  end
+end
+
