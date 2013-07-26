69b031b7b7ea8e128979f7955b5b74afcd6d2100
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-26 01:31:37 -0400

    test for fractional votes method

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,4 +5,11 @@ describe Vote do
   it {should have_and_belong_to_many(:legislators)}
   it {should validate_presence_of(:roll_id)}
   it {should validate_uniqueness_of(:roll_id)}
+
+  it 'should return vote roll for fractional votes' do
+    vote = FactoryGirl.create(:vote)
+    popular_vote, popular_tally = Vote.fractional_vote(vote.voters, :pop_percent, vote.required)
+    expect popular_vote.length.to eq(100)
+    expect popular_tally["Nay"].to exist
+  end
 end
