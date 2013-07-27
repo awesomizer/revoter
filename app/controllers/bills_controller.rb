37fccf3e3cb6ff6244a8ae6500fabed16171b91f
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-27 03:43:36 -0400

    refactor get and create bill methods

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,51 +1,16 @@
 class BillsController < ApplicationController
 
   def find
-    api = CongressApi.new
-    sun_bill_id = params[:bill_type] + params[:bill_number] + "-" + params[:congress]
-    results = api.get_bill(sun_bill_id)
-    results = results["results"][0]
-    @bill = Bill.find_or_create_by_sun_bill_id(results["bill_id"]) do |b|
-                        b.official_title = results["official_title"] 
-                        b.nicknames = results["nickname"] 
-                        b.summary_short = results["summary_short"] 
-                        b.last_vote_at = results["last_vote_at"] 
-                        b.last_action = results["last_action"] 
-                        b.vote_list = results["votes"]
-                        b.urls = results["urls"]
-    end
-      @votes = votes(@bill.vote_list)
-      render :show
+    sun_bill_id = [params[:bill_type] + params[:bill_number] + "-" + params[:congress]]
+    bill = Bill.get_bills(sun_bill_id)
+    @bill = bill[0]
+    @votes = Vote.get_votes(@bill.vote_list) # need to prep parameter as array
+    render :show
   end
 
   def show
     @bill = Bill.find(params[:id])
     @votes = @bill.votes
-  end
-
-
-  private
-
-  def votes vote_list
-    api = CongressApi.new
-    @votes = []
-    vote_list.each do |v|
-      if v["chamber"] == "senate"
-        results = api.get_vote(v["roll_id"])
-        results = results["results"][0]
-        vote = Vote.find_or_create_by_roll_id(results["roll_id"]) do |v|
-                            v.question = results["question"] 
-                            v.required = results["required"] 
-                            v.result = results["result"] 
-                            v.vote_type = results["vote_type"]
-                            v.breakdown = results["breakdown"]
-                            v.voters = results["voters"]
-                            v.bill_id = @bill.id
-        end
-        @votes << vote
-      end
-    end
-    @votes
   end
 
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -10,4 +10,31 @@ class Bill < ActiveRecord::Base
   has_many :votes
 
   validates_presence_of :sun_bill_id, :uniqueness => :true
+
+  def self.get_bills sun_bill_id_array
+    bill_list = []
+    sun_bill_id_array.each do |id|
+      bill = self.find_or_initialize_by_sun_bill_id(id)
+      if bill.new_record?
+        bill = self.create_bill(bill, id)
+      end
+      bill_list << bill
+    end
+    bill_list
+  end
+
+  def self.create_bill bill, sun_bill_id
+    api = CongressApi.new
+    results = api.get_bill(sun_bill_id)
+    results = results["results"][0]
+    bill.official_title = results["official_title"] 
+    bill.nicknames = results["nickname"] 
+    bill.summary_short = results["summary_short"] 
+    bill.last_vote_at = results["last_vote_at"] 
+    bill.last_action = results["last_action"] 
+    bill.vote_list = results["votes"]
+    bill.urls = results["urls"]
+    bill.save
+  end
+
 end
