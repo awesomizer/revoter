a681af6a25f3b08c69f46de19bfca87757d3fbe7
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-27 17:56:25 -0400

    tests for bill creation methods

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -3,4 +3,15 @@ require 'spec_helper'
 describe Bill do
   it {should have_many(:votes)}
   it {should validate_presence_of(:sun_bill_id)}
+
+  it 'should find existing bills given an array of roll_ids' do
+    bill = Bill.get_bills('s744-113')
+    expect bill.official_title.to eq('Best Bill')
+  end
+
+  it 'should create a bill if no bill already exists' do
+    bill = Bill.get_bills('s1243-113')
+    expect bill.sun_bill_id.to eq('s1243-113')
+  end
+
 end
