a53b9c84a8f91c327b1446d8d8373aacd6d60081
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-30 01:00:51 -0400

    remove comment

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -4,7 +4,7 @@ class BillsController < ApplicationController
     sun_bill_id = [params[:bill_type] + params[:bill_number] + "-" + params[:congress]]
     bill = Bill.get_bills(sun_bill_id)
     @bill = bill[0]
-    @votes = Vote.get_votes(@bill.vote_list) # need to prep parameter as array
+    @votes = Vote.get_votes(@bill.vote_list)
     render :show
   end
 
