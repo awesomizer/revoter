551d2abdc7c3f062348e2fb03ecb191ffbd0c256
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-30 01:03:01 -0400

    add conditional to sort new from updating records re state attribute

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,3 +1,4 @@
+require 'debugger'
 class Legislator < ActiveRecord::Base
 
   attr_accessible :bioguide_id,
@@ -19,19 +20,23 @@ class Legislator < ActiveRecord::Base
 
   validates :bioguide_id, presence: true, uniqueness: true
   
-  def self.create_or_update legislator
-    l = Legislator.find_or_initialize_by_bioguide_id(legislator["bioguide_id"])
+  def self.create_or_update voter
+    l = Legislator.find_or_initialize_by_bioguide_id(voter["bioguide_id"])
     if l.id.nil? || l.updated_at > Time.current - 1.months
-        l.state_code = legislator["state"]
-        l.title = legislator["title"]
-        l.chamber = legislator["chamber"]
-        l.state_rank = legislator["state_rank"]
-        l.state_name = legislator["state_name"]
-        l.first_name = legislator["first_name"]
-        l.nickname = legislator["nickname"]
-        l.last_name = legislator["last_name"]
-        l.party = legislator["party"]
-        l.state_id = State.find_by_code(legislator["state"]).id
+        l.state_code = voter["state"]
+        l.title = voter["title"]
+        l.chamber = voter["chamber"]
+        l.state_rank = voter["state_rank"]
+        l.state_name = voter["state_name"]
+        l.first_name = voter["first_name"]
+        l.nickname = voter["nickname"]
+        l.last_name = voter["last_name"]
+        l.party = voter["party"]
+        if voter["state"]
+          l.state_id = State.find_by_code(voter["state"]).id
+        else
+          l.state_id = State.find_by_code(voter["state_code"]).id
+        end
         l.save
     end
     return l  
