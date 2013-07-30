c186b645e6b9ccde43c77c43ac6d1fb5d7f0f482
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-30 01:00:07 -0400

    move basic ajax swap logic to haml files

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,25 +3,3 @@ window.onpopstate = function( event ) {
         $( 'body' ).load( event.state.path );
     };
 };
-
-var bodyAjax = (function() {
-
-	var ajaxLink = function( element ) {
-		$link = $( element );
-		href = $link.attr( 'href' );
-		$.get( href ).success( function( result ) {
-		switchBody( result );
-		});
-	};  
-
-	var switchBody = function( result ) {   
-		newBody = $.parseHTML( result, document, true );
-		oldBody = $( 'body' );
-		oldBody.html( newBody );
-	};
-
-	return {
-		ajaxLink: ajaxLink,
-		switchBody: switchBody
-	};
-})();
