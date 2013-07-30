c62e3d4337b1a3c132b1fb93bec0b273497c1b58
Christian Smith-Socaris <christian.working@gmail.com>
2013-07-30 01:08:29 -0400

    refactor bill model tests

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -5,13 +5,16 @@ describe Bill do
   it {should validate_presence_of(:sun_bill_id)}
 
   it 'should find existing bills given an array of roll_ids' do
-    bill = Bill.get_bills('s744-113')
-    expect bill.official_title.to eq('Best Bill')
+    bill = FactoryGirl.create(:bill)
+    gotten_bill = Bill.get_bills(['s744-113'])
+    expect(gotten_bill[0].official_title).to eq('Best Bill')
   end
 
-  it 'should create a bill if no bill already exists' do
-    bill = Bill.get_bills('s1243-113')
-    expect bill.sun_bill_id.to eq('s1243-113')
+  # this is returning true for new_bill.  not sure how to structure this, since it's supposed to be creating a new bill when one doesn't exist.  
+  pending 'should create a bill if no bill already exists' do
+    bill = FactoryGirl.create(:bill)
+    new_bill = Bill.get_bills(['s1243-113'])
+    expect(new_bill[0].id).to be > 0
   end
 
 end
