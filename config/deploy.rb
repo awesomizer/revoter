4ba311c5761f408cee4da02ae2d086534b9da42c
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-01 19:08:13 -0400

    add rvm/capistrano and rvm related variables and tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,10 @@
-require "bundler/capistrano"
+# require "bundler/capistrano" - having problems with missing /shared/bundle path
+require "rvm/capistrano"
+
+set :rvm_ruby_string, :local        # use the same ruby as used locally for deployment
+
+before 'deploy', 'rvm:install_rvm'  # update RVM
+before 'deploy', 'rvm:install_ruby'
 
 set :application, "revoter.us"
 set :repository,  "https://github.com/awesomizer/revoter.git"
@@ -9,11 +15,11 @@ set :scm, :git # You can set :scm explicitly or Capistrano will make an intellig
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :keep_releases, 5
 
-role :web, "apache2"                          # Your HTTP server, Apache/etc
-role :app, "passenger"                          # This may be the same as your `Web` server
-role :db,  "postgresql", :primary => true # This is where Rails migrations will run
+#role :web, ""                          # Your HTTP server, Apache/etc
+#role :app, ""                          # This may be the same as your `Web` server
+#role :db,  "", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
-server "www.revoter.us", :app, :web, :db, :primary => true
+server "173.255.226.98", :app, :web, :db, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -29,15 +35,22 @@ ssh_options[:forward_agent] = true
 
 default_run_options[:pty] = true  # Must be set for the password prompt
                                   # from git to work
-set :user, "deployer"  # The server's user for deploys
+set :user, "christian"  # The server's user for deploys
 set :use_sudo, false
 set :keep_releases, 5
-set :scm_passphrase, ""  
+set :scm_username, "githubname" # insert these just before running tasks on server, then delete
+set :scm_passphrase, "githubpass"  # insert these just before running tasks on server, then delete
+# Capistrano::CLI.password_prompt " => "
 
+# rvm settings to stop bundle --deployment
+set :bundle_dir, ''
+set :bundle_flags, '--system --quiet'
+
+# this copies in the app's config files that contain sensitive data
 namespace :db do
   task :db_config, :except => { :no_release => true }, :role => :app do
-    run "cp -f ~/privates/database.yml #{deploy_to}/config/database.yml"
-    run "cp -f ~/privates/application.yml #{deploy_to}/config/application.yml"
+    run "cp -f ~/privates/database.yml #{release_path}/config/database.yml"
+    run "cp -f ~/privates/application.yml #{release_path}/config/application.yml"
   end
 end
 
