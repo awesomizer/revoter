ba6b46975b258e79a0b8d30a980c4e293b2cc790
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-02 01:37:24 -0400

    setup for local precompile of assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,17 @@ require "bundler/capistrano"
 
 load 'deploy/assets'
 
+namespace :deploy do
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      %x{bundle exec rake assets:precompile}
+      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+      %x{bundle exec rake assets:clean}
+    end
+  end
+end
+
 set :application, "revoter.us"
 set :repository,  "https://github.com/awesomizer/revoter.git"
 set :branch, "master"
