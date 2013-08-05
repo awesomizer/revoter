233083241f92cba3cf62a25b49beeff9bdfcbf14
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-05 01:11:48 -0400

    add validation tests to vote spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe Vote do
+  
   it {should belong_to(:bill)}
   it {should have_and_belong_to_many(:legislators)}
   it {should validate_presence_of(:roll_id)}
   it {should validate_uniqueness_of(:roll_id)}
+  it {should validate_presence_of(:question)}
+  it {should validate_presence_of(:required)}
+  it {should validate_presence_of(:result)}
+  it {should validate_presence_of(:breakdown)}
+  it {should validate_presence_of(:voters)}
 
-# same problem as vote controller, calling vote model methods in turn call State.find_by_code, which I don't know how to mock.  
+# same problem as vote controller, calling vote model methods in turn calls State.find_by_code, which I don't know how to mock.  
   pending 'should return vote roll for fractional votes' do
     vote = FactoryGirl.create(:vote)
     popular_vote, popular_tally = Vote.fractional_vote(vote.voters, :pop_percent, vote.required)
