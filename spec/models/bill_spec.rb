1c4d5dea6df8bc35b2521acd1b243ce64d4ff3c1
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-04 21:19:26 -0400

    add pending test for get_bills method + cosmetic

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -8,16 +8,15 @@ describe Bill do
   it {should validate_presence_of(:summary_short)}
 
   it 'should find existing bills given an array of roll_ids' do
-    bill = FactoryGirl.create(:bill)
-    gotten_bill = Bill.get_bills(['s744-113'])
-    expect(gotten_bill[0].official_title).to eq('Best Bill')
+    bill = FactoryGirl.create( :bill )
+    gotten_bill = Bill.get_bills( ['s744-113'] )
+    expect( gotten_bill[0].official_title ).to eq( 'Best Bill' )
   end
 
-  # this is returning true for new_bill.  not sure how to structure this, since it's supposed to be creating a new bill when one doesn't exist.  
-  pending 'should create a bill if no bill already exists' do
-    bill = FactoryGirl.create(:bill)
-    new_bill = Bill.get_bills(['s1243-113'])
-    expect(new_bill[0].id).to be > 0
+  # figure out how to test the ability of self.get_bills to pass a bill not in the database to self.create_bill
+
+  pending 'should create a bill with a valid and initialized bill object and a sun_bill_id' do
+    bill = FactoryGirl.create( :bill )
   end
 
 end
