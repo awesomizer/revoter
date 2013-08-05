75c62ca59d0ef9efa796b5bf70ae4f424c12f929
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-04 21:20:00 -0400

    add test for get_recent_votes method

--- spec/models/congress_api_spec.rb
+++ spec/models/congress_api_spec.rb
@@ -2,31 +2,36 @@ require 'spec_helper'
 
 describe CongressApi do
 
-  # Mock the api in these tests?
+  # Mock the api in these tests
 
-  it 'should retrieve bill information with valid bill info' do
+  it 'retrieves bill information with valid bill info' do
       api = CongressApi.new 
       response = api.get_bill('s744-113')
       expect(response["results"][0]["bill_id"]).to eq('s744-113')
   end
 
-  it 'should display an error message with invalid bill info' do
+  it 'displays an error message with invalid bill info' do
       api = CongressApi.new 
       response = api.get_bill('hr3590-xxx')
       expect(response).to raise_error
   end
   
-  it 'should retrieve vote information with valid roll_id' do
+  it 'retrieves vote information with valid roll_id' do
       api = CongressApi.new
       response = api.get_vote('s396-2009')
       expect(response["results"][0]["question"]).not_to eq(nil)
   end
 
-  it 'should retrieve legislator information with a valid bioguide_id' do
+  it 'retrieves legislator information with a valid bioguide_id' do
       api = CongressApi.new
       response = api.get_legislator('B000944')
       expect(response["results"][0]["state"]).not_to eq(nil)
   end
 
+  it 'retrieves an array of recent votes' do
+      api = CongressApi.new
+      response = api.get_recent_votes( 'senate' )
+      expect( response.length ).to be > 1
+  end
 
 end
