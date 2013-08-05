d64a91dc4155ea44a25da9c30cb57f26ed909636
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-05 01:11:21 -0400

    add validation tests to legislator spec

--- spec/models/legislator_spec.rb
+++ spec/models/legislator_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 describe Legislator do
 
+  it {should belong_to(:state)}
   it {should have_and_belong_to_many(:votes)}
   it {should validate_presence_of(:bioguide_id)}
   it {should validate_uniqueness_of(:bioguide_id)}
+  it {should validate_presence_of(:state_code)}
+  it {should validate_presence_of(:chamber)}
+  it {should validate_presence_of(:first_name)}
+  it {should validate_presence_of(:last_name)}
+  it {should validate_presence_of(:party)}
 
   context 'GET #create_or_update' do
 
@@ -22,7 +28,7 @@ describe Legislator do
       expect(legislator.updated_at).to be < (Time.now - 1.weeks)
     end
 
-    it 'should not update legislators if they have been updated in less than a month' do
+    pending 'should not update legislators if they have been updated in less than a month' do
       leg = FactoryGirl.create(:legislator, updated_at: (Time.now - 2.weeks))
       legislator = Legislator.create_or_update(leg)
       expect(legislator.updated_at).to be > (Time.now - 1.weeks)
