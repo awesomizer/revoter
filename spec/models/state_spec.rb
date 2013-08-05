4eaf23df31804c51be1e20159e45939cd96e41de
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-05 01:11:32 -0400

    add validation tests to state spec

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe State do
   it {should have_many(:legislators)} 
+  it {should validate_presence_of(:code)} 
+  it {should validate_uniqueness_of(:code)} 
+  it {should validate_presence_of(:name)} 
+  it {should validate_presence_of(:population)} 
 end
