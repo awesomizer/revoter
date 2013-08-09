1f7c5a12210fcc24512f2109ee7dfab71c2ec770
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-09 02:52:01 -0400

    whitespace

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -15,7 +15,6 @@ class Vote < ActiveRecord::Base
     votes = []
     roll_id_array.each do |id|
       vote = self.find_or_initialize_by_roll_id( id )
-#debugger      
       if vote.new_record?
         vote = self.create_vote( vote, id ) # refactor this method to pass a list of votes to create votes and only hit the api once
       end
@@ -26,43 +25,43 @@ class Vote < ActiveRecord::Base
 
   def self.get_recent_votes chamber
     api = CongressApi.new
-    recent_votes = api.get_recent_votes(chamber) 
+    recent_votes = api.get_recent_votes( chamber ) 
   end
 
   def self.vote_positions
-    ["Yea", "Nay", "Present", "Not Voting"] 
+    [ "Yea", "Nay", "Present", "Not Voting" ] 
   end
 
   def self.fractional_vote voters, system, required
     votes = {}
-    tally = {"Result" => "", "Yea" => 0.0, "Nay" => 0.0, "Present" => 0.0, "Not Voting" => 0.0}
+    tally = { "Result" => "", "Yea" => 0.0, "Nay" => 0.0, "Present" => 0.0, "Not Voting" => 0.0 }
     voters.each_value do |v|
-      state = State.find_by_code(v["voter"]["state"])
-      weight = state.send(system.to_sym) / 2 
-      pretty_weight = sprintf('%.2f', weight)
+      state = State.find_by_code( v["voter"]["state"] )
+      weight = state.send( system.to_sym ) / 2 
+      pretty_weight = sprintf( '%.2f', weight )
       voter = v["voter"]["bioguide_id"]
-      votes.merge!({voter => pretty_weight})
-      tally[v["vote"]] += weight 
+      votes.merge!( {voter => pretty_weight} )
+      tally[ v["vote"] ] += weight 
     end
 
     tally = tally_formatter tally
-    tally = vote_result(tally, required)
+    tally = vote_result( tally, required )
     return votes, tally
   end
 
   def self.integer_vote voters, system, required
     votes = {}
-    tally = {"Result" => "", "Yea" => 0, "Nay" => 0, "Present" => 0, "Not Voting" => 0}
+    tally = { "Result" => "", "Yea" => 0, "Nay" => 0, "Present" => 0, "Not Voting" => 0 }
     voters.each_value do |v|
-      state = State.find_by_code(v["voter"]["state"])
-      vote = state.send(system.to_sym).to_i
-      weight = int_weight_calculator(vote, v)
+      state = State.find_by_code( v["voter"]["state"] )
+      vote = state.send( system.to_sym ).to_i
+      weight = int_weight_calculator( vote, v )
       voter = v["voter"]["bioguide_id"]
-      votes.merge!({voter => weight})
-      tally[v["vote"]] += weight 
+      votes.merge!( {voter => weight} )
+      tally[ v["vote"] ] += weight 
     end
 
-    tally = vote_result(tally, required)
+    tally = vote_result( tally, required )
     return votes, tally
   end
 
@@ -71,9 +70,8 @@ private
 
   def self.create_vote vote, roll_id # roll_id is nil
     api = CongressApi.new
-    results = api.get_vote(roll_id)
+    results = api.get_vote( roll_id )
     results = results["results"][0]
-#debugger    
     vote.roll_id = results["roll_id"]
     vote.question = results["question"] 
     vote.required = results["required"] 
@@ -81,7 +79,7 @@ private
     vote.vote_type = results["vote_type"]
     vote.breakdown = results["breakdown"]
     vote.voters = results["voters"]
-    bill = Bill.get_bills([results["bill"]["bill_id"]])
+    bill = Bill.get_bills( [ results["bill"]["bill_id"] ] )
     vote.bill_id = bill[0].id
     vote.save
     return vote
@@ -89,11 +87,11 @@ private
 
   def self.int_weight_calculator vote, v
     if vote == 1
-      vote = single_vote(v["voter"]["state_rank"], vote)
+      vote = single_vote( v["voter"]["state_rank"], vote )
     elsif vote % 2 == 0
       vote = vote / 2
     else vote % 2 == 1
-      vote = odd_vote(v["voter"]["state_rank"], vote)
+      vote = odd_vote( v["voter"]["state_rank"], vote )
     end
   end
 
@@ -107,18 +105,18 @@ private
 
   def self.odd_vote rank, vote
     if rank == "senior"
-      vote = (vote + 1) / 2
+      vote = ( vote + 1 ) / 2
     else
-      vote = (vote - 1) / 2
+      vote = ( vote - 1 ) / 2
     end
   end
 
   def self.tally_formatter tally
     tally.each do |t, n|
       if tally[t].is_a? Float
-        tally[t] = sprintf('%.2f', n)
-        tally[t].gsub!(/^0+/, "")
-        tally[t].gsub!(/0$/, "")
+        tally[t] = sprintf( '%.2f', n )
+        tally[t].gsub!( /^0+/, "" )
+        tally[t].gsub!( /0$/, "" )
       end
     end
     tally
