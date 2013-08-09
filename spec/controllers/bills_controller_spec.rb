6b92646cf3cddb58fd3ce97adb31e96af6f9c1e3
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-09 04:33:35 -0400

    taking specs off pending

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -6,13 +6,13 @@ describe BillsController do
 
 # rspec doesn't like the vote_list hash in the factory for some reason.  It spits an error that you can't mass-assign protected variables and lists the keys for the vote_list hash.    
 
-    pending 'retrieves bill information and assign it @bill' do
+    it 'retrieves bill information and assign it @bill' do
       bill = FactoryGirl.create(:bill)
       get :find, {bill_type: "s", bill_number: "744", congress: "113"}
       assigns(:bill).should eq(bill)
     end
 
-    pending 'retrieves an array of vote information and assign it to @votes' do
+    it 'retrieves an array of vote information and assign it to @votes' do
       bill = FactoryGirl.create(:bill)
       get :find, {bill_type: "s", bill_number: "744", congress: "113"}
       assigns(:votes).size.should == 1
@@ -28,7 +28,7 @@ describe BillsController do
       assigns(:bill).should eq(bill)
     end
 
-# The association in the bill factory seems to be wrong.  expectation is finding 0 votes assigned to @votes
+# The association in the bill factory is being recognized (can call .votes), but it is returning nil for votes.
     pending 'retrieves vote information and assigns it to @votes' do
       bill = FactoryGirl.create(:bill)
       get :show, {id: 5}
