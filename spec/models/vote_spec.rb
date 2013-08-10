b48cfce1085280541fb234d3605aee8e4a25aef5
Christian Smith-Socaris <christian.working@gmail.com>
2013-08-09 21:26:52 -0400

    fix incorrect expectation on fractional vote spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -11,12 +11,12 @@ describe Vote do
   it {should validate_presence_of(:result)}
   it {should validate_presence_of(:breakdown)}
   it {should validate_presence_of(:voters)}
-
-# same problem as vote controller, calling vote model methods in turn calls State.find_by_code, which I don't know how to mock.  
+  
+  # pending because of problems seeding states data.
   pending 'should return vote roll for fractional votes' do
     vote = FactoryGirl.create(:vote)
     popular_vote, popular_tally = Vote.fractional_vote(vote.voters, :pop_percent, vote.required)
-    expect popular_vote.length.to eq(100)
+    expect (popular_vote.length).to eq(3)
     expect popular_tally["Nay"].to exist
   end
 end
